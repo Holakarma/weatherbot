d849935da44e9a7665ec8f5a32c3ded7a0cd13a6
Holakarma <holakarmos@gmail.com>
2026-03-10 19:01:18 +0500

    src

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { Telegraf } from 'telegraf';
+import { ConfigService } from './src/config.service';
+
+const bootstrap = (configService) => {
+    const bot = new Telegraf(configService.get(BOT_TOKEN));
+    const app = express();
+
+    bot.start((ctx) => ctx.reply('Бот запущен'));
+
+    app.use(express.json());
+
+    const secretPath = `/telegraf/${BOT_TOKEN}`;
+    app.use(secretPath, (req, res) => {
+        bot.handleUpdate(req.body, res);
+    });
+    app.get('/', (req, res) => {
+        res.send('OK');
+    });
+
+    async function start() {
+        const webhookUrl = `${RENDER_EXTERNAL_URL}${secretPath}`;
+
+        await bot.telegram.setWebhook(webhookUrl);
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`);
+            console.log(`Webhook: ${webhookUrl}`);
+        });
+    }
+
+    start()
+        .then(() => {
+            console.log('bot started succesfully');
+        })
+        .catch((err) => {
+            console.error('Startup error:', err);
+            process.exit(1);
+        });
+};
+
+bootstrap(new ConfigService());
---
+++ src/config.service.js
@@ -0,0 +1,33 @@
+import { config } from 'dotenv';
+import 'dotenv/config';
+
+export class ConfigService {
+    constructor() {
+        const { error, parsed } = config();
+        if (error) {
+            throw new Error(`Failed to load .env variables: ${error.message}`);
+        }
+        if (!parsed) {
+            throw new Error(`Failed to load .env variables`);
+        }
+        this.config = parsed;
+        if (process.env.RENDER === 'true') {
+            console.log('render.com deployment');
+            this.config.DOMAIN = process.env.RENDER_EXTERNAL_URL;
+        }
+        this.validateConfig(this.config);
+    }
+
+    validateConfig(config) {
+        if (!config.BOT_TOKEN) throw Error('BOT_TOKEN is required');
+        if (!config.DOMAIN) throw new Error('DOMAIN is required');
+    }
+
+    get(key) {
+        const res = this.config[key];
+        if (!res) {
+            throw new Error(`Failed to get .env variable: ${key}`);
+        }
+        return res;
+    }
+}
